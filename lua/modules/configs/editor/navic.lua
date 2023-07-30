f554e57ea7513e24a95c09b54631544f4d4e7a7b
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 <yu.carl@outlook.com>
2023-07-30 22:24:40 +0100

    improve navic

--- lua/modules/configs/editor/navic.lua
+++ lua/modules/configs/editor/navic.lua
@@ -5,61 +5,57 @@ return function()
 	end
 
 	local exclude_filetype = {
-		"help",
-		"dashboard",
-		"packer",
-		"neogitstatus",
-		"NvimTree",
-		"Trouble",
-		"alpha",
-		"lir",
-		"Outline",
-		"spectre_panel",
-		"toggleterm",
-		"qf",
-		"VistaNvim",
+		["help"] = true,
+		["dashboard"] = true,
+		["packer"] = true,
+		["neogitstatus"] = true,
+		["NvimTree"] = true,
+		["Trouble"] = true,
+		["alpha"] = true,
+		["lir"] = true,
+		["Outline"] = true,
+		["spectre_panel"] = true,
+		["toggleterm"] = true,
+		["qf"] = true,
+		["VistaNvim"] = true,
 	}
 
+	local home = require("core.global").home
+
 	local excludes = function()
-		if vim.tbl_contains(exclude_filetype, vim.bo.filetype) or (vim.b[0].navic_client_id == nil) then
-			return true
-		end
-		if vim.api.nvim_win_get_config(0).relative ~= "" then
-			return true
-		end
-		return false
+		return exclude_filetype[vim.bo.filetype]
+			or vim.b[0].navic_client_id == nil
+			or vim.api.nvim_win_get_config(0).relative ~= ""
 	end
 
-	local clean_filepath = function(filepath)
-		local file_path_clean = filepath:gsub(vim.fn.expand("~/"), " ~/")
-		local file_name = vim.fn.expand("%:t")
-		file_path_clean = file_path_clean:gsub("/" .. file_name, "")
+	local clean_filepath = function(path)
+		if path:find(home, 1, true) == 1 then
+			path = " ~" .. path:sub(#home + 1)
+		end
 		local max_width = math.floor((vim.fn.winwidth(0)) * 0.5)
-		local path_width = file_path_clean:len()
+		local path_width = path:len()
 		if path_width > max_width then
-			file_path_clean = file_path_clean:sub(1, max_width) .. "..."
+			path = path:sub(1, max_width) .. "…"
 		else
-			file_path_clean = file_path_clean:sub(1, path_width)
+			path = path:sub(1, path_width)
 		end
-		return file_path_clean
+		return path
 	end
 
-	local get_icon = function()
+	local get_filetype_icon = function()
 		local file_extension = vim.fn.expand("%:e")
 		local file_name = string.lower(vim.fn.expand("%:t"))
 		local icons_ok, icons = pcall(require, "nvim-web-devicons")
 		if not icons_ok then
 			return "/"
 		else
-			local icon_color_ok, icon_color = pcall(icons.get_icon_color, file_name, file_extension)
-			if not icon_color_ok or icon_color == nil then
+			local icon_color_ok, icon, color = pcall(icons.get_icon_color, file_name, file_extension)
+			if not icon_color_ok or icon == nil or icon == "" then
 				return ""
-			else
-				local icon, color = require("nvim-web-devicons").get_icon_color(file_name, file_extension)
-				local hl_group_name = "WinbarIcon_" .. file_extension
-				vim.api.nvim_set_hl(0, hl_group_name, { fg = color })
-				return string.format("%%#%s#%s%%*", hl_group_name, icon)
 			end
+			local hl_group_name = "WinbarIcon_" .. (file_extension ~= "" and file_extension or file_name)
+			vim.api.nvim_set_hl(0, hl_group_name, { fg = color })
+			return string.format("%%#%s#%s%%*", hl_group_name, icon)
 		end
 	end
 
@@ -67,17 +63,15 @@ return function()
 		if excludes() then
 			return
 		end
-		if status_ok then
-			local filetype_icon = " " .. get_icon() .. " "
-			local file_path_clean = clean_filepath(vim.fn.expand("%:p"))
-			local file_name = vim.fn.expand("%:t")
-			local separator = " 〉"
-			local location = "%{%v:lua.require'nvim-navic'.get_location()%}"
-			local value = string.format("%s%s%s%s%s", file_path_clean, filetype_icon, file_name, separator, location)
-			local set_ok, _ = pcall(vim.api.nvim_set_option_value, "winbar", value, { scope = "local" })
-			if not set_ok then
-				return
-			end
+		local filetype_icon = " " .. get_filetype_icon() .. " "
+		local file_path_clean = clean_filepath(vim.fn.fnamemodify(vim.fn.expand("%:p"), ":h"))
+		local file_name = vim.fn.expand("%:t")
+		local separator = " 〉"
+		local location = "%{%v:lua.require'nvim-navic'.get_location()%}"
+		local value = string.format("%s%s%s%s%s", file_path_clean, filetype_icon, file_name, separator, location)
+		local set_ok, _ = pcall(vim.api.nvim_set_option_value, "winbar", value, { scope = "local" })
+		if not set_ok then
+			return
 		end
 	end
 
@@ -86,7 +80,7 @@ return function()
 		if file_path == "" then
 			return
 		end
-		local filetype_icon = " " .. get_icon() .. " "
+		local filetype_icon = " " .. get_filetype_icon() .. " "
 		if filetype_icon == "  " then -- if filetype icon not available, then no need to toggle winbar
 			return nil
 		end
@@ -107,9 +101,8 @@ return function()
 	})
 	-- This autocmd is used to show winbar even for those buffer without lsp attached
 	-- TODO: Better handler popup
-	local group = vim.api.nvim_create_augroup("LspAttach_navic", {})
 	vim.api.nvim_create_autocmd({ "DirChanged", "BufWinEnter", "BufFilePost" }, {
-		group = "LspAttach_navic",
+		group = vim.api.nvim_create_augroup("LspAttach_navic", { clear = true }),
 		callback = function()
 			if string.sub(vim.api.nvim_buf_get_name(0), 1, 1) == "/" and vim.wo.winbar == "" then
 				-- otherwise, there is a ">" above separator
