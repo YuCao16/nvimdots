7888fa97fbfdafd03f90569117b5fca81dd101b9
Yu <yu.cao@qmul.ac.uk>
2024-07-19 11:45:54 +0100

    update autotag

--- lua/modules/configs/editor/autotag.lua
+++ lua/modules/configs/editor/autotag.lua
@@ -1,5 +1,10 @@
 return function()
 	require("modules.utils").load_plugin("nvim-ts-autotag", {
+		opts = {
+			enable_close = true, -- Auto close tags
+			enable_rename = true, -- Auto rename pairs of tags
+			enable_close_on_slash = false, -- Auto close on trailing </
+		},
 		filetypes = {
 			"html",
 			"javascript",
