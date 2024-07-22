0e44d69704e72d8c5c2b9433d0ee4bafdf12b04d
Yu Cao <yucao@supports-MacBook-Air.local>
2024-07-22 11:28:56 +0100

    disable scrollview diagnostic

--- lua/modules/configs/ui/scrollview.lua
+++ lua/modules/configs/ui/scrollview.lua
@@ -5,10 +5,10 @@ return function()
 		scrollview_mode = "virtual",
 		excluded_filetypes = { "NvimTree", "terminal", "nofile", "neo-tree" },
 		winblend = 0,
-		signs_on_startup = { "diagnostics", "folds", "marks", "search", "spell" },
-		diagnostics_error_symbol = icons.diagnostics.Error,
-		diagnostics_warn_symbol = icons.diagnostics.Warning,
-		diagnostics_info_symbol = icons.diagnostics.Information,
-		diagnostics_hint_symbol = icons.diagnostics.Hint,
+		-- signs_on_startup = { "diagnostics", "folds", "marks", "search", "spell" },
+		-- diagnostics_error_symbol = icons.diagnostics.Error,
+		-- diagnostics_warn_symbol = icons.diagnostics.Warning,
+		-- diagnostics_info_symbol = icons.diagnostics.Information,
+		-- diagnostics_hint_symbol = icons.diagnostics.Hint,
 	})
 end
