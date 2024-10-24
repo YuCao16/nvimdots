b2010b49e8304a09be00b4d7f1a3a57210374a2f
Yu Cao <yucao@supports-MacBook-Air.local>
2024-10-24 10:53:56 +0100

    update claude model

--- lua/modules/configs/tool/avante.lua
+++ lua/modules/configs/tool/avante.lua
@@ -3,7 +3,7 @@ return function()
 		provider = "claude",
 		claude = {
 			endpoint = "https://api.anthropic.com",
-			model = "claude-3-5-sonnet-20240620",
+			model = "claude-3-5-sonnet-20241022",
 			temperature = 0,
 			max_tokens = 4096,
 		},
